37970dd6ab095a04505c5d9380259271181391e4
mrashutoshkrsingh <mrashutoshkrsingh@gmail.com>
2022-02-19 10:12:33 +0530

    schema and frontend pages created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,15 @@
+import { Route } from "react-router-dom";
 import "./App.css";
+import ChatPage from "./pages/ChatPage";
+import HomePage from "./pages/HomePage";
 
 function App() {
-  return <div className="App">This is an App</div>;
+  return (
+    <div className="App">
+      <Route exact path="/" component={HomePage} />
+      <Route exact path="/chats" component={ChatPage} />
+    </div>
+  );
 }
 
 export default App;
